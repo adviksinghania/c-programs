11a906b4de145d65fa8c0a0f1a56d0be40437ffe
Advik Singhania <adviksinghania@gmail.com>
2020-11-25 23:32:24 +0530

    Modified Strings Program 17

--- Strings/string17.c
+++ Strings/string17.c
@@ -1,4 +1,4 @@
-//Print the words starting with vowels
+//Print the number of vowels and consonants in each word
 #include <stdio.h>
 #include <string.h>
 
@@ -9,15 +9,21 @@ int main() {
     // strcat() functions concatenates the first parameter
     strcat(str, " ");// with the second parameter and stores it into 1st parameter.
     int p=0;
-    printf("Output:\n");
+    printf("Output:\n\n");
     for (int i=0; i < (int)strlen(str); i++) {
         if (str[i] == ' ') {
-            if (str[p] == 'a' || str[p] == 'e' || str[p] == 'i' || str[p] == 'o' || str[p] == 'u' || str[p] == 'A' || str[p] == 'E' || str[p] == 'I' || str[p] == 'O' || str[p] == 'U') {
-                char word[100]="";
-                for(int j=p; j<i; j++)
-                    strncat(word, &str[j], 1);
-                printf("%s, ", word);
+            int vow=0, cons=0;
+            for(int j=p; j<i; j++) {
+                if((str[j] >= 'A' && str[j] <= 'Z') || (str[j] >= 'a' && str[j] <= 'z')) {
+                    if (str[j] == 'a' || str[j] == 'e' || str[j] == 'i' || str[j] == 'o' || str[j] == 'u' || str[j] == 'A' || str[j] == 'E' || str[j] == 'I' || str[j] == 'O' || str[j] == 'U')
+                        vow++;
+                    else
+                        cons++;
+                    printf("%c", str[j]);
+                }
             }
+            printf(":\nNumber of vowels: %d\n", vow);
+            printf("Number of consonants: %d\n\n", cons);
             p=i+1;
         }
     }
