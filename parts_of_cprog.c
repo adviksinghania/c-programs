e68aab7f2fcfdb0aa6ce6879b4ec3d4515672f2d
Anandbrewery <101007408+Anandbrewery@users.noreply.github.com>
2022-10-02 16:07:13 +0530

    Add files via upload

---
+++ parts_of_cprog.c
@@ -0,0 +1,11 @@
+/*
+U.ANANDHAKUMAR
+EC21B1087
+*/
+// - single line comment
+#include<stdio.h>// preprocessor statement or header statement.
+//stdio.h - header file
+void main()
+{
+  printf("Hello world\n");
+}
