6cf46cfbf0032750744b740bf729df205c05ff60
Pratik Jallan <74804069+Yukino2002@users.noreply.github.com>
2022-10-04 12:43:02 +0530

    Update count_frequency.c

--- Arrays/count_frequency.c
+++ Arrays/count_frequency.c
@@ -1,24 +1,99 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(){
-    // declare an array 
-    int arr[] = {1, 5, 3, 4, 5, 6, 7, 8, 5, 5, 1, 5, 1, 5, 5, 5, 5};
-    // declare a variable to store the length of the array
-    int n = sizeof(arr)/sizeof(arr[0]);
+#define SIZE 200
 
-    // find the frequency of each element
-    for(int i = 0; i < n; i++){
-        int count = 1;
-        for(int j = i+1; j < n; j++){
-            if(arr[i] == arr[j]){
-                count++;
-                arr[j] = -1;
-            }
-        }
-        if(arr[i] != -1){
-            printf("%d occurs %d times\n", arr[i], count);
-        }
+struct DataItem
+{
+    int data;
+    int key;
+};
+
+struct DataItem *hashArray[SIZE];
+struct DataItem *dummyItem;
+struct DataItem *item;
+
+int hashCode(int key)
+{
+    return key % SIZE;
+}
+
+struct DataItem *search(int key)
+{
+    // get the hash
+    int hashIndex = hashCode(key);
+
+    // move in array until an empty
+    while (hashArray[hashIndex] != NULL)
+    {
+
+        if (hashArray[hashIndex]->key == key)
+            return hashArray[hashIndex];
+
+        // go to next cell
+        ++hashIndex;
+
+        // wrap around the table
+        hashIndex %= SIZE;
     }
-    
-    return 0;
+
+    return NULL;
+}
+
+void insert(int key, int data)
+{
+
+    struct DataItem *item = (struct DataItem *)malloc(sizeof(struct DataItem));
+    item->data = data;
+    item->key = key;
+
+    // get the hash
+    int hashIndex = hashCode(key);
+
+    // move in array until an empty or deleted cell
+    while (hashArray[hashIndex] != NULL && hashArray[hashIndex]->key != -1)
+    {
+        // go to next cell
+        ++hashIndex;
+
+        // wrap around the table
+        hashIndex %= SIZE;
+    }
+    hashArray[hashIndex] = item;
+}
+
+void display()
+{
+    int i = 0;
+
+    for (i = 0; i < SIZE; i++)
+    {
+        if (hashArray[i] != NULL) printf(" %d occurs %d times\n", hashArray[i]->key, hashArray[i]->data);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    dummyItem = (struct DataItem *)malloc(sizeof(struct DataItem));
+    dummyItem->data = -1;
+    dummyItem->key = -1;
+
+    // declare an array of 20 integers and initialize
+    int arr[10] = {1, 12, 43, 1, 1, 7, 1, 29, 29, 59};
+
+    // count the number of times each integer appears in the array
+    for (int i = 0; i < 10; i++)
+    {
+        item = search(arr[i]);
+        if (item == NULL)
+            insert(arr[i], 1);
+        else
+            item->data = item->data + 1;
+    }
+
+    display();
 }
